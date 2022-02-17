b49a48805cc6bbb5eb643a9fdaed3109f2f04df6
edgar <edgar.alba>
2022-02-17 15:26:03 -0600

    cambios en inicio

--- server/routes/inicio.js
+++ server/routes/inicio.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { verificaToken } = require('../middlewares/autenticacion');
 const app = express();
 
-app.all('/', verificaToken, (req, res) => {
+app.all('/', (req, res) => {
     return res.status(200).json({
         ok: true,
         msg: 'Inicio del backend ',
